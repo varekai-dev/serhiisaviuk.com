b3856fbf1b8643d146a08c6eaed537c290b59aa2
Serhii <s.saviuk@apiko.com>
2024-06-28 17:33:30 +0300

    Update packages

--- components/ThemeSwitch.jsx
+++ components/ThemeSwitch.jsx
@@ -1,18 +1,19 @@
-import React, { useContext } from 'react';
-import { AppContext } from './context/AppContext';
-import { useRouter } from 'next/router';
+import React, { useContext } from 'react'
+import { AppContext } from './context/AppContext'
+import { useRouter } from 'next/router'
 
 function ThemeSwitch({ text }) {
-	const router = useRouter();
-	const { theme, setTheme } = useContext(AppContext);
-	const handleClick = () => {
-		setTheme(theme === 'light' ? 'dark' : 'light');
-	};
-	return (
-		<a className="theme-switcher" onClick={handleClick}>
-			{text}
-		</a>
-	);
+    const router = useRouter()
+    const { theme, setTheme } = useContext(AppContext)
+    const handleClick = e => {
+        e.preventDefault()
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+    return (
+        <a className="theme-switcher" onClick={handleClick}>
+            {text}
+        </a>
+    )
 }
 
-export default ThemeSwitch;
+export default ThemeSwitch
